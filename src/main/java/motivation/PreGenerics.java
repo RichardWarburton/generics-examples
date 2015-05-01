cf672ba159852c880fd5b5abab084679504afd7f
Richard Warburton <richard.warburton@gmail.com>
2015-05-01 10:50:47 +0100

    add motivating example

---
+++ src/main/java/motivation/PreGenerics.java
@@ -0,0 +1,23 @@
+package motivation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PreGenerics
+{
+    public static void main(String[] args)
+    {
+        List list = new ArrayList();
+        list.add("a");
+        list.add("b");
+        list.add(1);
+
+        Iterator it = list.iterator();
+        while (it.hasNext())
+        {
+            final String value = (String) it.next();
+            System.out.println(value);
+        }
+    }
+}
---
+++ src/main/java/motivation/WithGenerics.java
@@ -0,0 +1,20 @@
+package motivation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WithGenerics
+{
+    public static void main(String[] args)
+    {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        for (final String string : list)
+        {
+            System.out.println(string);
+        }
+    }
+}
