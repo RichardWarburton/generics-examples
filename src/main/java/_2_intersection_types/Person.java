c10aa5d71248485e3a62e6e128ea01d7ce9cd441
Richard Warburton <richard.warburton@gmail.com>
2015-05-22 15:45:48 +0300

    refactor person comments

--- src/main/java/_2_intersection_types/Person.java
+++ src/main/java/_2_intersection_types/Person.java
@@ -6,14 +6,15 @@ public class Person
 {
     public static void main(String[] args) throws IOException
     {
-        //RandomAccessFile randomAccess = new RandomAccessFile("src/main/resources/person", "rw");
         DataInputStream stream = new DataInputStream(new FileInputStream("src/main/resources/person"));
-
         Person person = read(stream);
         System.out.println(person);
 
-        /*person = read(randomAccess);
-        System.out.println(person);*/
+        /*
+        RandomAccessFile randomAccess = new RandomAccessFile("src/main/resources/person", "rw");
+        person = read(randomAccess);
+        System.out.println(person);
+        */
     }
 
     private static Person read(DataInputStream source)
