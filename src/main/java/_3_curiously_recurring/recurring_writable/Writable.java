51bab0ec037993d572d0bbf1cda865f5d02c20a0
raoulDoc <raoul@urma.com>
2016-03-13 15:54:30 +0000

    fix typo missing T

--- src/main/java/_3_curiously_recurring/recurring_writable/Writable.java
+++ src/main/java/_3_curiously_recurring/recurring_writable/Writable.java
@@ -1,6 +1,6 @@
 package _3_curiously_recurring.recurring_writable;
 
-public interface Writable<T extends Writable> {
+public interface Writable<T extends Writable<T>> {
     T writeInt(int number);
     T writeString(String message);
 }
